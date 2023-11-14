93f67b127e84c4c3529e32c1b656541f9a5e4098
Recyclosaucisse <105792894+Recyclosaucisse@users.noreply.github.com>
2023-11-14 15:17:08 +0100

    Update TilioArduino.ino

--- TilioArduino.ino
+++ TilioArduino.ino
@@ -1,54 +1,48 @@
-#include <SPI.h>
-#include <LoRa.h>
 #include <M5Stack.h>
+#include <M5LoRa.h>
+#include <SPI.h>
 
-int counter = 0;
 
 void setup() {
-  M5.begin();             // Init M5Stack.
-  M5.Power.begin();       // Init power
-  M5.Lcd.setTextSize(2);  // Set the text size to 2
-  M5.Lcd.println("Detection");
-  M5.Lcd.setCursor(0,25);  // Position the cursor at (0,25).
-  M5.Lcd.println("Status: \nValue: ");
-  pinMode(36, INPUT);  // Set pin 36 to input mode.
-  Serial.begin(9600);
-  
-  while (!Serial);
-  Serial.println("LoRa Sender");
-  if (!LoRa.begin(868E6)) {
-    Serial.println("Starting LoRa failed!");
-    while (1);
-  }
+    M5.begin();
+    // Override the default CS, reset, and IRQ pins (optional)
+    LoRa.setPins();  // Default set CS, reset, IRQ pin
+    M5.Lcd.setTextSize(2); // Set the text size to 2
+    M5.Lcd.println("Detection");
+    M5.Lcd.setCursor(0, 25); // Postion of the cursor at (0,25)
+    M5.Lcd.println("Status: \nValue: \nEmission :");
+    pinMode(36, INPUT);
+
+    // Frequency in Hz (433E6, 866E6, 915E6)
+    LoRa.begin(866E6);
 }
 
 void loop() {
-  M5.Lcd.fillRect(90, 25, 180, 50,BLACK);  // Draw a black rectangle 180 by 50 at (90,25).
+    M5.Lcd.fillRect(90, 25, 180, 75, BLACK);  // Draw a black rectangle 180 by 50 at (90,25).
+    static uint32_t counter;
+    // Detecting something
     if (digitalRead(36) == 1) 
-    {  // If pin 36 reads a value of 1. Movement detected
+    {
       M5.Lcd.setCursor(95, 25);
-      M5.Lcd.print("MOUVEMENT !!!!");
-      M5.Lcd.setCursor(95, 45);
-      M5.Lcd.print("1");
-      // send packet
+      M5.Lcd.println("Mouvement");
+      M5.Lcd.setCursor(95, 50);
+      M5.Lcd.println("1");
+      M5.Lcd.setCursor(95, 70);
+      M5.Lcd.println("J'envoie R");
+          // Sending packets
       LoRa.beginPacket();
       LoRa.print("Mouvement");
-      LoRa.print(counter);
       LoRa.endPacket();
-      counter++;
-      delay(5000);
-    } 
-else 
-  {
-    M5.Lcd.setCursor(95, 25);
-    M5.Lcd.print("PAS DE MOUVEMENT !");
-    M5.Lcd.setCursor(95, 45);
-    M5.Lcd.print("0");
-    LoRa.beginPacket();
-    LoRa.print("hello ");
-    LoRa.print(counter);
-    LoRa.endPacket();
-    counter++;
-    delay(5000);
-  }
+    }
+    else 
+    {
+      M5.Lcd.setCursor(95, 25);
+      M5.Lcd.println("Pas de mouvement");
+      M5.Lcd.setCursor(95, 50);
+      M5.Lcd.println("0");
+      M5.Lcd.setCursor(95, 70);
+      M5.Lcd.println("J'envoie R");
+    }
+    // Add a short delay to avoid reading too frequently
+    delay(1000);
 }
