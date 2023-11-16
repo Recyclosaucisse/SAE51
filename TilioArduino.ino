01b45691d9062c3fac95df539bd1baf4e02e274e
Recyclosaucisse <105792894+Recyclosaucisse@users.noreply.github.com>
2023-11-16 09:35:42 +0100

    Update TilioArduino.ino
    Changement de PIN + Emission

--- TilioArduino.ino
+++ TilioArduino.ino
@@ -3,6 +3,9 @@
 #include <SPI.h>
 
 
+const int pinCapteur = 16;  // Ajustez cela en fonction du numéro de broche que vous utilisez pour votre capteur
+unsigned long tempsDebut = 0;
+
 void setup() {
     M5.begin();
     // Override the default CS, reset, and IRQ pins (optional)
@@ -11,27 +14,31 @@ void setup() {
     M5.Lcd.println("Detection");
     M5.Lcd.setCursor(0, 25); // Postion of the cursor at (0,25)
     M5.Lcd.println("Status: \nValue: \nEmission :");
-    pinMode(36, INPUT);
+    pinMode(pinCapteur, INPUT);
 
     // Frequency in Hz (433E6, 866E6, 915E6)
-    LoRa.begin(866E6);
+    LoRa.begin(868E6);
 }
 
 void loop() {
-    M5.Lcd.fillRect(90, 25, 180, 75, BLACK);  // Draw a black rectangle 180 by 50 at (90,25).
+    M5.Lcd.fillRect(90, 25, 180, 100, BLACK);  // Draw a black rectangle 180 by 50 at (90,25).
     static uint32_t counter;
+
+    // Read the state of the sensor
+    int etatCapteur = digitalRead(pinCapteur);
+
     // Detecting something
-    if (digitalRead(36) == 1) 
+    if (etatCapteur == HIGH) 
     {
       M5.Lcd.setCursor(95, 25);
       M5.Lcd.println("Mouvement");
       M5.Lcd.setCursor(95, 50);
       M5.Lcd.println("1");
       M5.Lcd.setCursor(95, 70);
-      M5.Lcd.println("J'envoie R");
+      M5.Lcd.println("J'envoie");
           // Sending packets
       LoRa.beginPacket();
-      LoRa.print("Mouvement");
+      LoRa.print("1");
       LoRa.endPacket();
     }
     else 
@@ -44,5 +51,5 @@ void loop() {
       M5.Lcd.println("J'envoie R");
     }
     // Add a short delay to avoid reading too frequently
-    delay(1000);
+    delay(5000);
 }
